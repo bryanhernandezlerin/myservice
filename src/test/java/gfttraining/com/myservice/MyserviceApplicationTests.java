2f6a7b5b907d4ff72d193327a43ecfde47183bf0
Bryan <bnhn@gft.com>
2023-05-02 13:12:39 +0000

    initial commit

---
+++ src/test/java/gfttraining/com/myservice/MyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package gfttraining.com.myservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
